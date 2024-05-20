04530a1dce1b6d13b9c6b0be0f128a55607293ef
huajibin <hujiabin@thunisoft.com>
2024-05-20 22:19:54 +0800

    csv cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// rcli csv -i input.csv -o output.json --header -d ','
+
+use clap::Parser;
+use rcil::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow:: Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
